ff6bb9969e4145f288b9fd8ee7dcd2f9ce64e6cc
Matheus <mrochasandim75@gmail.com>
2024-03-26 19:21:43 -0300

    estudo vue aula 01

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,27 @@
 <script setup>
+import { ref } from 'vue'
+
+const name = ref('')
+const age = ref()
+const user = ref()
+
+function createUser() {
+  user.value = {
+    name: name.value,
+    age: age.value
+  }
+}
 
 </script>
 
 <template>
   <div>
-    Olá Mundo!
+    <!-- two way data bind -->
+    <input type="text" placeholder="Nome" v-model="name">
+    <input type="number" placeholder="Idade" v-model="age">
+    <button @click="createUser">Cadastrar</button>
+    <p v-show="user?.name">Nome: {{ user?.name }}</p>
+    <p v-if="user?.age">Idade: {{ user?.age }}</p>
     
   </div>
 </template>
